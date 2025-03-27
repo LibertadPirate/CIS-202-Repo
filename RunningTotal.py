1d1faa8780038e25be13177d1065316e7ceba0b0
LibertadPirate <125110540+LibertadPirate@users.noreply.github.com>
2025-03-26 19:49:28 -0500

    Add files via upload
    Kevin from Montego Bay is calculating a running total for you.

---
+++ RunningTotal.py
@@ -0,0 +1,12 @@
+# 100% of Communication is just copy and pasting someone else's work.
+
+num_inputs = int(input('How much numba dem, bredda? '))
+
+n_2_gedda = 0.0
+
+for poop in range(num_inputs):
+    numba = float(input(f"give dem numba {poop +1}: "))
+    n_2_gedda += numba
+    print (f"Yuh total so far after {poop +1}: {n_2_gedda:.2f}")
+
+print (f"\nYuh total: {n_2_gedda:.2f}")
